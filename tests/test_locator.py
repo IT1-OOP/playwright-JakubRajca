94358e8b3865f74e9ea12df1ebcd5810e2967c01
Jakub Rajča <94049646+JakubRajca@users.noreply.github.com>
2025-04-03 13:11:47 +0200

    ne, asi ne

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -1,13 +1,16 @@
 import os
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
+import urllib.parse
 
 def test_page_title():
-    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/index.html"))
+    cesta = os.path.abspath("index.html")
+    cesta = urllib.parse.unquote(cesta)
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
-        page.goto(f"file://{file_path}")
-        title = page.title()
-        print(title)
+        page.goto(f"file://{cesta}")
+        nadpis_1 = page.locator('h1').first
+        expect(nadpis_1).to_be_visible()
+        browser.close()
         
